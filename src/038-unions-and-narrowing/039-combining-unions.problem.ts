f40f2341045baa00dcff759d190fd8b46a4854c9
Matt Pocock <mattpocockvoice@gmail.com>
2023-08-15 11:47:01 +0100

    Added exercise on combining unions

---
+++ src/038-unions-and-narrowing/039-combining-unions.problem.ts
@@ -0,0 +1,21 @@
+type HttpCode = "400" | "401" | "404" | "500" | "200" | "201" | "204";
+
+const handleErrorCase = (code: string) => {
+  // An imaginary function where we only handle the errors
+
+  type test = Expect<Equal<typeof code, "400" | "401" | "404" | "500">>;
+};
+
+const handleSuccessCase = (code: string) => {
+  // An imaginary function where we only handle the success cases
+
+  type test = Expect<Equal<typeof code, "200" | "201" | "204">>;
+};
+
+const handleAllCase = (code: HttpCode) => {
+  // An imaginary function where we handle all the cases
+
+  type test = Expect<
+    Equal<typeof code, "200" | "201" | "204" | "400" | "401" | "404" | "500">
+  >;
+};
---
+++ src/038-unions-and-narrowing/039-combining-unions.solution.ts
@@ -0,0 +1,24 @@
+type SuccessCode = "200" | "201" | "204";
+type ErrorCode = "400" | "401" | "404" | "500";
+
+type HttpCode = SuccessCode | ErrorCode;
+
+const handleErrorCase = (code: ErrorCode) => {
+  // An imaginary function where we only handle the errors
+
+  type test = Expect<Equal<typeof code, "400" | "401" | "404" | "500">>;
+};
+
+const handleSuccessCase = (code: SuccessCode) => {
+  // An imaginary function where we only handle the success cases
+
+  type test = Expect<Equal<typeof code, "200" | "201" | "204">>;
+};
+
+const handleAllCase = (code: HttpCode) => {
+  // An imaginary function where we handle all the cases
+
+  type test = Expect<
+    Equal<typeof code, "200" | "201" | "204" | "400" | "401" | "404" | "500">
+  >;
+};
