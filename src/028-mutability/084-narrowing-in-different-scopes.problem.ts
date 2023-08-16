a3f19eb71a131b9fbb6b793d8f8f54a90b3e3457
Matt Pocock <mattpocockvoice@gmail.com>
2023-08-16 15:06:59 +0100

    2023-08-16T14:06:59.054Z

---
+++ src/028-mutability/084-narrowing-in-different-scopes.problem.ts
@@ -0,0 +1,42 @@
+import { expect, it } from "vitest";
+
+const findUsersByName = (
+  searchParams: { name?: string },
+  users: {
+    id: string;
+    name: string;
+  }[],
+) => {
+  if (searchParams.name) {
+    return users.filter((user) => user.name.includes(searchParams.name));
+  }
+
+  return users;
+};
+
+it("Should find the exact name", () => {
+  const result = findUsersByName(
+    {
+      name: "Bob",
+    },
+    [
+      {
+        id: "1",
+        name: "Bob",
+      },
+      {
+        id: "2",
+        name: "Alice",
+      },
+    ],
+  );
+
+  expect(result).toEqual([
+    {
+      id: "1",
+      name: "Bob",
+    },
+  ]);
+
+  type test = Expect<Equal<typeof result, { id: string; name: string }[]>>;
+});
---
+++ src/028-mutability/084-narrowing-in-different-scopes.solution.2.ts
@@ -0,0 +1,43 @@
+import { expect, it } from "vitest";
+
+const findUsersByName = (
+  searchParams: { name?: string },
+  users: {
+    id: string;
+    name: string;
+  }[],
+) => {
+  const name = searchParams.name;
+  if (name) {
+    return users.filter((user) => user.name.includes(name));
+  }
+
+  return users;
+};
+
+it("Should find the exact name", () => {
+  const result = findUsersByName(
+    {
+      name: "Bob",
+    },
+    [
+      {
+        id: "1",
+        name: "Bob",
+      },
+      {
+        id: "2",
+        name: "Alice",
+      },
+    ],
+  );
+
+  expect(result).toEqual([
+    {
+      id: "1",
+      name: "Bob",
+    },
+  ]);
+
+  type test = Expect<Equal<typeof result, { id: string; name: string }[]>>;
+});
