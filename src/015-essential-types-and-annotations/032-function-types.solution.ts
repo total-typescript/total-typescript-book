16aaee424fb9558286bb6891795546ca24ca6aec
Matt Pocock <mattpocockvoice@gmail.com>
2023-08-30 12:59:51 +0100

    2023-08-30T11:59:51.224Z

--- src/015-essential-types-and-annotations/032-function-types.solution.ts
+++ src/015-essential-types-and-annotations/032-function-types.solution.ts
@@ -1,1 +1,35 @@
-// TODO - pass a callback to a function
+type User = {
+  id: string;
+  name: string;
+};
+
+const modifyUser = (
+  user: User[],
+  id: string,
+  makeChange: (user: User) => User,
+) => {
+  return user.map((u) => {
+    if (u.id === id) {
+      return makeChange(u);
+    }
+    return u;
+  });
+};
+
+const users: User[] = [
+  { id: "1", name: "John" },
+  { id: "2", name: "Jane" },
+];
+
+modifyUser(users, "1", (user) => {
+  return { ...user, name: "Waqas" };
+});
+
+modifyUser(
+  users,
+  "1",
+  // @ts-expect-error
+  (user) => {
+    return { ...user, name: 123 };
+  },
+);
