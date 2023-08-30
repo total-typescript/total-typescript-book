bdc407908b0c8cff7a617531343edab8f798e434
Matt Pocock <mattpocockvoice@gmail.com>
2023-08-30 15:44:38 +0100

    2023-08-30T14:44:38.906Z

---
+++ src/016.5-ide-superpowers/044-rename-symbol.explainer.ts
@@ -0,0 +1,12 @@
+const users = [
+  { id: "1", name: "Robin" },
+  { id: "2", name: "Dennis" },
+  { id: "3", name: "Sara" },
+];
+
+// Imagine this function was 10x bigger, with 10x more references to `id`
+
+// How do we change id to userId?
+const filterUsersById = (id: string) => {
+  return users.filter((user) => user.id === id);
+};
---
+++ src/016.5-ide-superpowers/045-resetting-the-typescript-server.explainer.ts
@@ -0,0 +1,5 @@
+// When configs change
+
+// Sometimes TS can get out of sync because it doesn't notice a file changed
+
+// It's always good to have in your back pocket
