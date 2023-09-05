ebf67159b72acaf225216c53d04a9ac89ce038b1
Matt Pocock <mattpocockvoice@gmail.com>
2023-09-05 10:50:56 +0100

    Added 082

--- src/020-objects/082-property-key-type.solution.ts
+++ src/020-objects/082-property-key-type.solution.ts
@@ -1,5 +1,35 @@
-type TypeWithIndexSignature = {
-  [key: PropertyKey]: {
-    id: string;
-  };
+import { expect, it } from "vitest";
+
+const hasKey = (obj: object, key: PropertyKey) => {
+  return obj.hasOwnProperty(key);
 };
+
+it("Should work on string keys", () => {
+  const obj = {
+    foo: "bar",
+  };
+
+  expect(hasKey(obj, "foo")).toBe(true);
+  expect(hasKey(obj, "bar")).toBe(false);
+});
+
+it("Should work on number keys", () => {
+  const obj = {
+    1: "bar",
+  };
+
+  expect(hasKey(obj, 1)).toBe(true);
+  expect(hasKey(obj, 2)).toBe(false);
+});
+
+it("Should work on symbol keys", () => {
+  const fooSymbol = Symbol("foo");
+  const barSymbol = Symbol("bar");
+
+  const obj = {
+    [fooSymbol]: "bar",
+  };
+
+  expect(hasKey(obj, fooSymbol)).toBe(true);
+  expect(hasKey(obj, barSymbol)).toBe(false);
+});
