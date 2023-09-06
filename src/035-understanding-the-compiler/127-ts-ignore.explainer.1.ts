240fb285cd507b7323ffbfea6c5badc605786397
Matt Pocock <mattpocockvoice@gmail.com>
2023-09-06 14:51:19 +0100

    2023-09-06T13:51:19.077Z

---
+++ src/035-understanding-the-compiler/127-ts-ignore.explainer.1.ts
@@ -0,0 +1,26 @@
+import { expect, it } from "vitest";
+
+const handleFormData = (e: SubmitEvent) => {
+  e.preventDefault();
+
+  // @ts-ignore
+  const data = new FormData(e.target);
+  const value = Object.fromEntries(data.entries());
+  return value;
+};
+
+it("Should handle a form submit", () => {
+  const form = document.createElement("form");
+  form.innerHTML = `
+    <input name="name" value="John Doe" />
+  `;
+
+  form.onsubmit = (e) => {
+    const value = handleFormData(e);
+    expect(value).toEqual({ name: "John Doe" });
+  };
+
+  form.requestSubmit();
+
+  expect.assertions(1);
+});
---
+++ src/035-understanding-the-compiler/128-ts-expect-error.explainer.ts
@@ -0,0 +1,27 @@
+import { expect, it } from "vitest";
+
+const handleFormData = (e: SubmitEvent) => {
+  e.preventDefault();
+  const data = new FormData(
+    // @ts-expect-error
+    e.target,
+  );
+  const value = Object.fromEntries(data.entries());
+  return value;
+};
+
+it("Should handle a form submit", () => {
+  const form = document.createElement("form");
+  form.innerHTML = `
+    <input name="name" value="John Doe" />
+  `;
+
+  form.onsubmit = (e) => {
+    const value = handleFormData(e);
+    expect(value).toEqual({ name: "John Doe" });
+  };
+
+  form.requestSubmit();
+
+  expect.assertions(1);
+});
