9a695bf504933065b7e2953209d955637d9db126
Matt Pocock <mattpocockvoice@gmail.com>
2023-09-06 14:45:09 +0100

    2023-09-06T13:45:09.926Z

--- src/035-understanding-the-compiler/126-global-typings-use-any.solution.1.ts
+++ src/035-understanding-the-compiler/126-global-typings-use-any.solution.1.ts
@@ -1,16 +1,14 @@
-import { expect, it } from "vitest";
+import { it } from "vitest";
 
 const getObj = () => {
   const obj: {
     a: number;
     b: number;
-  } = JSON.parse('{ "a": 123, "c": 456 }');
+  } = JSON.parse('{ "a": 123, "b": 456 }');
 
   return obj;
 };
 
 it("Should return an obj", () => {
   const obj = getObj();
-
-  expect(obj.b).toEqual(456);
 });
--- src/035-understanding-the-compiler/126-global-typings-use-any.solution.2.ts
+++ src/035-understanding-the-compiler/126-global-typings-use-any.solution.2.ts
@@ -1,16 +1,15 @@
-import { expect, it } from "vitest";
+// import '@total-typescript/ts-reset'
+import { it } from "vitest";
 
 const getObj = () => {
   const obj: {
     a: number;
     b: number;
-  } = JSON.parse('{ "a": 123, "c": 456 }');
+  } = JSON.parse('{ "a": 123, "b": 456 }');
 
   return obj;
 };
 
 it("Should return an obj", () => {
   const obj = getObj();
-
-  expect(obj.b).toEqual(456);
 });
