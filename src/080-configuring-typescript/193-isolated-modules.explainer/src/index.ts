188e152dc52085409ce9bf6b347b7c95cc78cc05
Matt Pocock <mattpocockvoice@gmail.com>
2023-09-12 15:34:49 +0100

    2023-09-12T14:34:49.871Z

--- src/080-configuring-typescript/193-isolated-modules.explainer/src/index.ts
+++ src/080-configuring-typescript/193-isolated-modules.explainer/src/index.ts
@@ -6,7 +6,9 @@ declare const enum Numbers {
 
 const example = Numbers.Zero;
 
-// TODO - find more examples that fix isolatedDeclarations
+// TODO - find more examples that are fixed by isolatedDeclarations
 
 // It was added to support Babel:
 // https://devblogs.microsoft.com/typescript/typescript-and-babel-7/
+
+// https://twitter.com/atcb/status/1701604609528307755
