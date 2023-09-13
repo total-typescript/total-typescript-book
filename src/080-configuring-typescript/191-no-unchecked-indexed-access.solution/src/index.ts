8f91ed71aa622233725d56da98e5ca051e08321e
Matt Pocock <mattpocockvoice@gmail.com>
2023-09-13 10:17:13 +0100

    2023-09-13T09:17:13.105Z

---
+++ src/080-configuring-typescript/191-no-unchecked-indexed-access.solution/src/index.ts
@@ -0,0 +1,5 @@
+const array = [1, 2, 3];
+
+const mightNotExist = array[3];
+
+type test = Expect<Equal<typeof mightNotExist, number | undefined>>;
--- src/080-configuring-typescript/192-tsconfig-bases.explainer.ts
+++ src/080-configuring-typescript/192-tsconfig-bases.explainer.ts
@@ -1,1 +1,3 @@
 // https://github.com/tsconfig/bases
+
+// Note that strictest is not what I recommend
