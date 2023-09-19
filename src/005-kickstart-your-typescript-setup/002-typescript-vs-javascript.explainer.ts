9eb1cab9f48cdcaec522c05838e6a74d1613639e
Matt Pocock <mattpocockvoice@gmail.com>
2023-09-19 14:34:18 +0100

    2023-09-19T13:34:18.569Z

---
+++ src/005-kickstart-your-typescript-setup/002-typescript-vs-javascript.explainer.ts
@@ -0,0 +1,1 @@
+// Explain autocomplete and type checking
---
+++ src/085-the-utils-folder/221-type-predicates.solution.ts
@@ -0,0 +1,15 @@
+import { expect, it } from "vitest";
+
+const isString = (input: unknown): input is string => {
+  return typeof input === "string";
+};
+
+it("Should be able to be passed to .filter and work", () => {
+  const mixedArray = [1, "hello", [], {}];
+
+  const stringsOnly = mixedArray.filter(isString);
+
+  type test1 = Expect<Equal<typeof stringsOnly, string[]>>;
+
+  expect(stringsOnly).toEqual(["hello"]);
+});
