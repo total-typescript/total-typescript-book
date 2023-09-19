c2ef3f2abf922cd1153c488dba5150a1c74750c7
Matt Pocock <mattpocockvoice@gmail.com>
2023-09-19 12:26:19 +0100

    2023-09-19T11:26:19.004Z

---
+++ src/085-the-utils-folder/213-intro-to-the-utils-folder.explainer.ts
@@ -0,0 +1,3 @@
+// Three levels of TypeScript complexity
+
+// Application Development, Library Development and Utils Folder Development
---
+++ src/085-the-utils-folder/214-generic-functions-without-inference.problem.ts
@@ -0,0 +1,22 @@
+const createStringMap = () => {
+  return new Map();
+};
+
+const numberMap = createStringMap<number>();
+
+numberMap.set("foo", 123);
+numberMap.set(
+  "bar",
+  // @ts-expect-error
+  true,
+);
+
+const objMap = createStringMap<{ a: number }>();
+
+objMap.set("foo", { a: 123 });
+
+objMap.set(
+  "bar",
+  // @ts-expect-error
+  { b: 123 },
+);
