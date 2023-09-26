68349add695d7e4024f5f5027fe88fbb86e93b8b
Matt Pocock <mattpocockvoice@gmail.com>
2023-09-26 14:13:40 +0100

    Reformat

--- src/016.5-ide-superpowers/045-basics-of-errors.explainer.ts
+++ src/016.5-ide-superpowers/045-basics-of-errors.explainer.ts
@@ -1,16 +1,19 @@
-// TypeScript sometimes warns you about things which will fail at runtime
+// TypeScript sometimes warns you about
+// things which will fail at runtime
 
 const a = null;
 
 a.toString();
 
-// But not everything it warns you about will fail at runtime
+// But not everything it warns you
+// about will fail at runtime
 
 const obj = {};
 
 obj.foo = "hello";
 
-// It will try to warn you as close to the source of the problem as possible
+// It will try to warn you as close to
+// the source of the problem as possible
 
 type MyUser = {
   name: string;
