05c1d1755bac209df7e7fed14e59a1e7876e925b
Matt Pocock <mattpocockvoice@gmail.com>
2023-10-04 15:42:12 +0300

    Added imports to utils folder section

--- src/085-the-utils-folder/215-generic-functions-without-inference.problem.ts
+++ src/085-the-utils-folder/215-generic-functions-without-inference.problem.ts
@@ -1,3 +1,5 @@
+import { Equal, Expect } from "@total-typescript/helpers";
+
 const createStringMap = () => {
   return new Map();
 };
--- src/085-the-utils-folder/215-generic-functions-without-inference.solution.ts
+++ src/085-the-utils-folder/215-generic-functions-without-inference.solution.ts
@@ -1,3 +1,5 @@
+import { Equal, Expect } from "@total-typescript/helpers";
+
 const createStringMap = <T>() => {
   return new Map<string, T>();
 };
