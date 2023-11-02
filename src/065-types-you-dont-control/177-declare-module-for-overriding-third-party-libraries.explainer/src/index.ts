1d82f7585a46306a9ddd46ba7e4288b4c1c433c2
Matt Pocock <mattpocockvoice@gmail.com>
2023-11-02 10:02:46 +0000

    Added type test to 177

--- src/065-types-you-dont-control/177-declare-module-for-overriding-third-party-libraries.explainer/src/index.ts
+++ src/065-types-you-dont-control/177-declare-module-for-overriding-third-party-libraries.explainer/src/index.ts
@@ -1,5 +1,8 @@
+import { type Equal, type Expect } from "@total-typescript/helpers";
 import { z } from "zod";
 
 const user = z.object({
   name: z.string(),
 });
+
+type Test = Expect<Equal<typeof user, any>>;
