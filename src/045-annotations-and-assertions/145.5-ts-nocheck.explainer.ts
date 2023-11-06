94991d93ce6c48aac2f387f750404151da874e3c
Matt Pocock <mattpocockvoice@gmail.com>
2023-11-06 10:57:47 +0000

    Fixed ts-nocheck section

--- src/045-annotations-and-assertions/145.5-ts-nocheck.explainer.ts
+++ src/045-annotations-and-assertions/145.5-ts-nocheck.explainer.ts
@@ -1,3 +1,27 @@
 // @ts-nocheck
 
-const ohDear: string = 123;
+import { expect, it } from "vitest";
+
+const handleFormData = (e: SubmitEvent) => {
+  e.preventDefault();
+
+  const data = new FormData(e.target);
+  const value = Object.fromEntries(data.entries());
+  return value;
+};
+
+it("Should handle a form submit", () => {
+  const form = document.createElement("form");
+  form.innerHTML = `
+    <input name="name" value="John Doe" />
+  `;
+
+  form.onsubmit = (e) => {
+    const value = handleFormData(e);
+    expect(value).toEqual({ name: "John Doe" });
+  };
+
+  form.requestSubmit();
+
+  expect.assertions(1);
+});
