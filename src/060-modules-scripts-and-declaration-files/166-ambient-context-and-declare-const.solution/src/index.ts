325029d4706ee62da6a8d8e55eedc9ae5f68ac6a
Matt Pocock <mattpocockvoice@gmail.com>
2023-11-07 11:04:54 +0000

    Slight change to a solution

--- src/060-modules-scripts-and-declaration-files/166-ambient-context-and-declare-const.solution/src/index.ts
+++ src/060-modules-scripts-and-declaration-files/166-ambient-context-and-declare-const.solution/src/index.ts
@@ -1,7 +1,9 @@
 import { Equal, Expect } from "@total-typescript/helpers";
 
 declare const DEBUG: {
-  getState(): { id: string };
+  getState: () => {
+    id: string;
+  };
 };
 
 const state = DEBUG.getState();
