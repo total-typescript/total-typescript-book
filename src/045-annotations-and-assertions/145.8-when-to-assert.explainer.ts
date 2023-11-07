fb7a664ee0f608c9cd1378f8fc3be733ef07ff26
Matt Pocock <mattpocockvoice@gmail.com>
2023-11-07 09:17:09 +0000

    Added 145.8 explainer text

---
+++ src/045-annotations-and-assertions/145.8-when-to-assert.explainer.ts
@@ -0,0 +1,10 @@
+// Explain when and when not to use unsafe assertions in TS
+
+// When there are upstream issues (in a library) causing strange TS errors
+
+// "When you know better than TypeScript"
+
+// But this isn't very helpful for folks who aren't confident in their TS skills yet
+
+// When you hit a horrible TS error, set a timer.
+// If you haven't solved it by the end of that timer, suppress the error.
