064c05049d41d169bbd42f73ae21dabdd915660f
Matt Pocock <mattpocockvoice@gmail.com>
2023-11-14 11:04:40 +0000

    Removed TODO's

--- src/080-configuring-typescript/178.5-isolated-modules.explainer/src/index.ts
+++ src/080-configuring-typescript/178.5-isolated-modules.explainer/src/index.ts
@@ -6,10 +6,5 @@ declare const enum Numbers {
 
 const example = Numbers.Zero;
 
-// TODO - find more examples that are fixed by isolatedDeclarations
-
 // It was added to support Babel:
 // https://devblogs.microsoft.com/typescript/typescript-and-babel-7/
-
-// TODO
-// https://twitter.com/atcb/status/1701604609528307755
